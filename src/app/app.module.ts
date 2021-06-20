d6fc60765adf309cd15a112d6f89b045ec3f8d2e
Jorge Leonardo Herrera Ortiz <herrera.jorge@correounivalle.edu.co>
2021-06-19 23:26:52 -0500

    feat(Módulos - segunda parte)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ContadorComponent } from './contador/contador.component';
 
+import { ContadorModule } from './contador/contador.module';
 import { HeroesModule } from './heroes/heroes.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ContadorComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
+    ContadorModule,
     HeroesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
